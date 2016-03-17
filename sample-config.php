890a2a49925be0b1d45c1251d5cc0a6e494deaf0
Ian MacKenzie <ian@quayzar.com>
2016-03-17 16:54:26 -0400

    Update sample-config.php

--- sample-config.php
+++ sample-config.php
@@ -23,7 +23,7 @@ define( 'ADDL_SUBDIR', '/additional_subdirectory' );
 define( 'DB_HOST', 		'' );
 define( 'DB_NAME', 		'' );
 define( 'DB_USER', 		'' );
-define( 'DB_PASSWORD',	'' );
+define( 'DB_PASSWORD',	  '' );
 
 /*
  * Environment-specific salts & keys
